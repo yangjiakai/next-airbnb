1a6745e4feef765fca98b443c3b36c405cf9ecff
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2024-01-03 23:32:39 +0900

    Add footer

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -2,6 +2,7 @@
 
 import { useTranslations } from "next-intl";
 import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 import { getAreas } from "@/api/areaApi";
 import { useEffect } from "react";
 export default function Home() {
@@ -23,6 +24,8 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         {t("home")}
       </main>
+      {/* Footer */}
+      <Footer />
     </>
   );
 }
